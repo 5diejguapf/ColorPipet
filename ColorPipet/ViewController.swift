cafe6ccdd17aeb298898a4d97915a9ee34f7f0d0
Sergei Shabalin <6f98v468t@mozmail.com>
2023-01-10 01:37:49 +0300

    fix of remarks

--- ColorPipet/ViewController.swift
+++ ColorPipet/ViewController.swift
@@ -13,10 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet var colorView: UIView!
     
     @IBOutlet var redValue: UILabel!
-    @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenValue: UILabel!
-    @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
     override func viewDidLoad() {
@@ -24,39 +25,46 @@ class ViewController: UIViewController {
         
         colorView.layer.cornerRadius = 10
         
-        redSlider.value = Float.random(in: 0.0...1.0)
-        greenSlider.value = Float.random(in: 0.0...1.0)
-        blueSlider.value = Float.random(in: 0.0...1.0)
-        
-        redValue.text = String(format: "%.2f", redSlider.value)
-        greenValue.text = String(format: "%.2f", greenSlider.value)
-        blueValue.text = String(format: "%.2f", blueSlider.value)
-        
         redSlider.tintColor = .red
         greenSlider.tintColor = .green
         blueSlider.tintColor = .blue
         
-        updateColorVuewBackground(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
+        initRandomColor()
+        updateLabelsText()
+        
+        updateColorVuewBackground()
     }
 
     // MARK: - IBActions
     @IBAction func redSliderValueChanged() {
         redValue.text = String(format: "%.2f", redSlider.value)
-        updateColorVuewBackground(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
+        updateColorVuewBackground()
     }
     @IBAction func greenSliderValueChanged() {
         greenValue.text = String(format: "%.2f", greenSlider.value)
-        updateColorVuewBackground(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
+        updateColorVuewBackground()
     }
     @IBAction func blueSliderValueChanged() {
         blueValue.text = String(format: "%.2f", blueSlider.value)
-        updateColorVuewBackground(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value)
+        updateColorVuewBackground()
     }
     
     // MARK: - private funcs
-    private func updateColorVuewBackground(red: Float, green: Float, blue: Float) {
-        let color = UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0)
+    private func updateColorVuewBackground() {
+        let color = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
         colorView.backgroundColor = color
+    }
+    
+    private func initRandomColor() {
+        redSlider.value = Float.random(in: 0.0...1.0)
+        greenSlider.value = Float.random(in: 0.0...1.0)
+        blueSlider.value = Float.random(in: 0.0...1.0)
+    }
+    
+    private func updateLabelsText() {
+        redValue.text = String(format: "%.2f", redSlider.value)
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        blueValue.text = String(format: "%.2f", blueSlider.value)
     }
 }
 
